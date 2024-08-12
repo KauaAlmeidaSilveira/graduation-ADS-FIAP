a57e360d00a800ee88a606cd9a2015977dc2a82b
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-08-12 20:24:28 -0300

    Interpolacao e println

---
+++ 2 - Semestre/MOBILE APPLICATION DEVELOPMENT/04 - ApresenteSe/ApresenteSe.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var age = 19;
+    var height = 1.69f // f -> Para representar float
+    var name = 'K'
+
+    println("Minha inicial e $name, tenho $age e minha altura e de %.2f".format(height))
+}
+
