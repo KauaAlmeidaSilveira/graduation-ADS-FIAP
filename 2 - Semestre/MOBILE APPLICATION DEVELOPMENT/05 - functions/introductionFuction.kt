553725b667f629c85a9ee73358d5894d23f04995
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-08-12 20:48:51 -0300

    update desafio

--- 2 - Semestre/MOBILE APPLICATION DEVELOPMENT/05 - functions/introductionFuction.kt
+++ 2 - Semestre/MOBILE APPLICATION DEVELOPMENT/05 - functions/introductionFuction.kt
@@ -1,7 +1,10 @@
 fun main() {
-    var a = 5;
-    var b = 10;
+    var result = somaDosQuadrados(5f, 10f)
+    println("A soma dos quadrados e igual a = $result")
+}
+
+fun somaDosQuadrados(a:Float, b:Float): Float{
     var sum = ((a*a) + (b*b));
-    println("A soma dos quadrados e igual a = $sum")
+    return sum;
 }
 
